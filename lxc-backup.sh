e919e1ef59288f29b622b5a3043487312dcf924d
Willem Oosting <woosting@users.noreply.github.com>
2017-02-10 23:03:17 +0100

    Update lxc-backup.sh

--- lxc-backup.sh
+++ lxc-backup.sh
@@ -5,7 +5,6 @@
 
 BACKUP_TARGET=~/
 BACKUP_PREFIX=$(date +%Y%m%d%H%M%S)
-TAR_EXCLUDE_PATTERNS=("/SNAPSHOTS /snaps")
 
 
 # INITIALISATION
@@ -14,27 +13,27 @@ LXC_PATH=$(lxc-config lxc.lxcpath)
 LXC_CONTAINERS=$(lxc-ls)
 LXC_CONTAINERS_ACTIVE=$(lxc-ls --active)
 
+
 # EXECUTION
 
-if [ 0 == ${#LXC_CONTAINERS[@]} ]; then
-    echo -e "No containers to archive found!"
-    exit 1;
-fi
+  # ERROR CHECKING
 
-if [ ${#LXC_CONTAINERS_ACTIVE[@]} -gt 0 ]; then
-    echo -e "---"
-    echo -e "${LXC_CONTAINERS_ACTIVE[@]}"
-    echo -e "---"
-    echo -e "Are still active (either running or frozen)!"
-#    exit 1;
-fi
+    if [ 0 == ${#LXC_CONTAINERS[@]} ]; then
+        echo -e "No containers to archive found!"
+        exit 1;
+    fi
 
-for LXC_CONTAINER in ${LXC_CONTAINERS}
-do
-    EXLUDES=()
-    for EXCLUDE in ${TAR_EXCLUDE_PATTERNS}
+    if [ ${#LXC_CONTAINERS_ACTIVE[@]} -gt 0 ]; then
+        echo -e "---"
+        echo -e "${LXC_CONTAINERS_ACTIVE[@]}"
+        echo -e "---"
+        echo -e "Are still active (either running or frozen)!"
+    #    exit 1;
+    fi
+  
+  # ACTUAL TARRING
+
+    for LXC_CONTAINER in ${LXC_CONTAINERS}
     do
-      EXCLUDES+=("--exclude='${LXC_CONTAINER}${EXCLUDE}'")
+        tar --numeric-owner --verbose -cf ${BACKUP_TARGET}/${BACKUP_PREFIX}-${LXC_CONTAINER}.tar.bz2 -C ${LXC_PATH} ${LXC_CONTAINER}
     done
-    tar --numeric-owner --verbose ${EXCLUDES[@]} -cf ${BACKUP_TARGET}/${BACKUP_PREFIX}-${LXC_CONTAINER}.tar.bz2 -C ${LXC_PATH} ${LXC_CONTAINER}
-done
